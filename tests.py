431a1f95c05333ec6fe631fa0b41b6ca0e52b8c1
bynn <bynnbynn@gmail.com>
2017-04-24 15:50:48 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -34,41 +34,43 @@ print('Herald Coordinates: ' +str(herald.getCoordinates()))
 done = False
 
 while not done:
-        for event in pygame.event.get():
-            keys = pygame.key.get_pressed()
-            if event.type == pygame.QUIT:
-                done = True
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                bullet = bulletclass2.Bullet(herald, BLACK)
-                bullet_list.add(bullet)
-                all_sprites_list.add(bullet)
-                print('Bullet Count: ' +str(bullet.count()))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            bullet = makeBullet(totalBulletCount)
+            totalBulletCount += 1
+            bullet_list.add(bullet)
+            all_sprites_list.add(bullet)
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        stuf.left()
+        print('Left: ' +str(stuf.getCoordinates()))
+    elif keys[pygame.K_RIGHT]:
+        stuf.right()
+        print('Right: ' +str(stuf.getCoordinates()))
+    elif keys[pygame.K_UP]:
+        stuf.up()
+        print('Up: ' +str(stuf.getCoordinates()))
+    elif keys[pygame.K_DOWN]:
+        stuf.down()
+        print('Down: ' + str(stuf.getCoordinates()))
+        # for bul in bullet_list:
+        #         if(pygame.sprite.collide_rect(bul, stuf)):
+        #                 stuf.health -= bul.damage
+        #                 print("Health:", stuf.health)
+        #                 all_sprites_list.remove(bul)
+        #                 bullet_list.remove(bul)
+        #                 if(stuf.health <= 0):
+        #                         all_sprites_list.remove(stuf)
+        #                         print("you suck")
 
-            
-                if bullet.rect.y > 400:
-                    bullet_list.remove(bullet)
-                    
-            '''elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    stuf.left()
-                    print('Left: ' +str(stuf.getCoordinates()))
-                elif event.key == pygame.K_RIGHT:
-                    stuf.right()
-                    print('Right: ' +str(stuf.getCoordinates()))
-                elif event.key == pygame.K_UP:
-                    stuf.up()
-                    print('Up: ' +str(stuf.getCoordinates()))
-                elif event.key == pygame.K_DOWN:
-                    stuf.down()
-                    print('Down: ' + str(stuf.getCoordinates()))'''
-            elif keys[pygame.K_UP]:
-                stuf.up()
-                    
-        all_sprites_list.update()
-        screen.fill(WHITE) 
-        all_sprites_list.draw(screen)
-        pygame.display.update()
 
-        clock.tick(30) 
-        
+    all_sprites_list.update()
+    screen.fill(WHITE)
+    all_sprites_list.draw(screen)
+    pygame.display.update()
+
+    clock.tick(30)
+
 pygame.quit()
