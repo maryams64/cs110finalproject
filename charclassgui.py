7a7d122ee3570c342f5b5ee8be854b9139864f19
maryams64 <maryam.soomro129@gmail.com>
2017-04-30 22:13:40 -0400

    Add files via upload

---
+++ charclassgui.py
@@ -0,0 +1,42 @@
+import pygame
+import os
+
+class char(pygame.sprite.Sprite):
+    "This class is the box"
+    def __init__(self, name, image):
+        super().__init__()
+        self.name = name
+        self.health = 100
+
+        self.image = pygame.image.load(os.path.join('images', image))
+        self.image = pygame.transform.scale(self.image, (200, 200))
+
+        self.rect = self.image.get_rect()
+        
+        self.rect.x = 50
+        self.rect.y = 300
+        '''self.lead_x_up = 0
+        self.lead_x_down = 0
+        self.lead_y_up = 0
+        self.lead_y_down = 0'''
+        self.sp = 2
+        #pygame.draw.rect(gameDisplay, red, [self.lead_x,self.lead_y, 10,10])
+
+
+    def left(self):
+        self.rect.x -= self.sp
+
+    def right(self):
+        self.rect.x += self.sp
+
+    def up(self):
+        self.rect.y -= self.sp
+
+    def down(self):
+        self.rect.y +=  self.sp
+
+    def getCoordinates(self):
+        return([self.rect.x,self.rect.y])
+    
+    def __str__(self):
+        return('heip')
