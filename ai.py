3a7d0b90b82b7f63745d8ca2a195f60d20b9483d
bynn <bynnbynn@gmail.com>
2017-05-04 21:57:19 -0400

    Add files via upload

---
+++ ai.py
@@ -0,0 +1,24 @@
+import pygame
+import os
+
+class combatAi(pygame.sprite.Sprite):
+    def __init__(self, level):
+        super().__init__()
+
+        if(level == 1):
+            self.image = pygame.image.load(os.path.join('images', 'bean.png'))
+        elif(level == 2):
+            self.image = pygame.image.load(os.path.join('images', 'zach.png'))
+        self.w = 100
+        self.h = 100
+        self.image = pygame.transform.scale(self.image, (self.w, self.h))
+        self.image = self.image.convert_alpha()
+
+
+        self.rect = self.image.get_rect()
+
+        self.rect.x = 350
+        self.rect.y = 0
+
+    def getCoordinates(self):
+        return([self.rect.x,self.rect.y])
